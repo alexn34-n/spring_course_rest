f2ca3d117702c37e02dd96552a1f10b3f1f27836
alexn34-n <alexn3469@mail.ru>
2021-09-04 11:54:14 +0300

    Удаление работника

--- src/main/java/com/alexn/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/alexn/spring/rest/controller/MyRESTController.java
@@ -52,4 +52,18 @@ public class MyRESTController {
 
     }
 
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployees(@PathVariable int id){
+
+        Employee employee=employeeService.getEmployee(id);
+        if(employee==null){
+
+            throw new NoSuchEmployeeException("There is no employee  with ID = "+
+                    id+ " in  Database");
+        }
+
+        employeeService.deleteEmployee(id);
+        return "Employee with ID = "+ id+ " was deleted";
+    }
+
 }
